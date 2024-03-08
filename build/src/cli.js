b17f0f9c43be436123a78b97550f863941ab0d53
marcomarca <55802648+marcomarca@users.noreply.github.com>
2024-03-08 09:41:21 -0400

    simple md output file

--- build/src/cli.js
+++ build/src/cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import convert from './main.js';
+import * as fs from 'fs/promises';
 const program = new Command();
 program.name('w2m');
 program.description('Convert Word documents to beautiful Markdown');
@@ -9,7 +10,8 @@ program
     .argument('<file>', 'The Word document to convert')
     .action(async (file) => {
     const md = await convert(file);
-    console.log(md);
+    await fs.writeFile('output.md', md);
+    console.log('Markdown saved to output.md');
 });
 program.parse();
 //# sourceMappingURL=cli.js.map
--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@
 
 import { Command } from 'commander';
 import convert from './main.js';
+import * as fs from 'fs/promises';
 
 const program = new Command();
 program.name('w2m');
@@ -9,9 +10,10 @@ program.description('Convert Word documents to beautiful Markdown');
 program
   .command('convert', { isDefault: true })
   .argument('<file>', 'The Word document to convert')
-  .action(async (file) => {
-    const md = await convert(file);
-    console.log(md);
+  .action(async (file: string) => {
+    const md: string = await convert(file);
+    await fs.writeFile('output.md', md);
+    console.log('Markdown saved to output.md');
   });
 
 program.parse();
